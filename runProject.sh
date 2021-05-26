519765c405bd65f89811b312388ecbf108e0b311
Lavanya <lavanyahm@gamil.com>
2021-05-26 12:04:41 +0530

    adding code to run for multiple configurations

--- runProject.sh
+++ runProject.sh
@@ -1,19 +1,64 @@
 #!/bin/bash
+#This file is to run cod with various combinations
 if [ "$1" != "" ]; then
-	./waf --run scratch/project >> outPut.log
+
+        numberOfRuns=$1
+
+	i=0
 	sleep 1
-	
-	./waf --run scratch/project >> outPut.log
+	# -lt is less than operator 
+  	#Iterate the loop until a less than 10 
+	while [ $i -lt $numberOfRuns ] 
+	do       
+		./waf --run "scratch/project    --n_nodes=16  --speed_min=0  --speed_max=10  " 
+		sleep 1   		
+		# increment the value 
+    		i=`expr $i + 1` 
+		echo $i
+	done 
+
+	i=0
 	sleep 1
+	# -lt is less than operator 
+  	#Iterate the loop until a less than 10 
+	while [ $i -lt $numberOfRuns ] 
+	do       
+		./waf --run "scratch/project    --n_nodes=64  --speed_min=10  --speed_max=30  " 
+		sleep 1   		
+		# increment the value 
+    		i=`expr $i + 1` 
+		echo $i
+	done 
+
+	i=0
+	sleep 1
+	# -lt is less than operator 
+  	#Iterate the loop until a less than 10 
+	while [ $i -lt $numberOfRuns ] 
+	do       
+		./waf --run "scratch/project    --n_nodes=64  --speed_min=30  --speed_max=40  " 
+		sleep 1   		
+		# increment the value 
+    		i=`expr $i + 1` 
+		echo $i
+	done 
+
+	i=0
+	sleep 1
+	# -lt is less than operator 
+  	#Iterate the loop until a less than 10 
+	while [ $i -lt $numberOfRuns ] 
+	do       
+		./waf --run "scratch/project    --n_nodes=64  --speed_min=40  --speed_max=50  " 
+		sleep 1   		
+		# increment the value 
+    		i=`expr $i + 1` 
+		echo $i
+	done 
 
 
-	./waf --run scratch/project >> outPut.log
-	sleep 1
+
 else
-	echo "Provide runNumbers  "
+	echo "Provide arguments :[ Numner of Runs]"
 fi	
-
-
-
-
 
